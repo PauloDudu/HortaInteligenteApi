8aecfc8f5b6fb2a072cb14a8661cdea7e424f51a
BrPauloEduardo <65795649+PauloDudu@users.noreply.github.com>
2024-04-12 01:03:27 -0300

    Add files via upload

--- src/database.js
+++ src/database.js
@@ -1,11 +1,15 @@
 const Pool = require("pg").Pool;
 
+// const pool = new Pool({
+//     host: "localhost",
+//     user: "postgres",
+//     database: "horta-inteligente",
+//     password: "123123",
+//     port: 5432
+// });
+
 const pool = new Pool({
-    host: "localhost",
-    user: "postgres",
-    database: "horta-inteligente",
-    password: "123123",
-    port: 5432
-});
+    connectionString: process.env.POSTGRES_URL ,
+})
 
 module.exports = pool;
